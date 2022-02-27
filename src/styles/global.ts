b6f69f80bda18d636fcee11398e8a5ff03487050
Michael Douglas Costa Silveira <mdsilveira@RDM503735.local>
2022-02-26 22:56:16 -0300

    changed url opticubalibretwo font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -36,11 +36,12 @@ export default createGlobalStyle`
 		url("src/fonts/Prata-Regular.ttf") format("truetype");
 	}
 
-	@font-face {
-		font-family: "O PTICubaLibreTwo";
-		src: url("src/fonts/OPTICubaLibreTwo.ttf") format("truetype"),
-			url("src/fonts/OPTICubaLibreTwo.woff") format("woff"),
-			url("src/fonts/OPTICubaLibreTwo.woff2") format("woff2");
-;
-	}
+	@font-face {font-family: "O PTICubaLibreTwo";
+    src: url("https://db.onlinewebfonts.com/t/d095a95a5dde87cfb124088b1bd4676e.eot"); /* IE9*/
+    src: url("https://db.onlinewebfonts.com/t/d095a95a5dde87cfb124088b1bd4676e.eot?#iefix") format("embedded-opentype"), /* IE6-IE8 */
+    url("https://db.onlinewebfonts.com/t/d095a95a5dde87cfb124088b1bd4676e.woff2") format("woff2"), /* chrome firefox */
+    url("https://db.onlinewebfonts.com/t/d095a95a5dde87cfb124088b1bd4676e.woff") format("woff"), /* chrome firefox */
+    url("https://db.onlinewebfonts.com/t/d095a95a5dde87cfb124088b1bd4676e.ttf") format("truetype"), /* chrome firefox opera Safari, Android, iOS 4.2+*/
+    url("https://db.onlinewebfonts.com/t/d095a95a5dde87cfb124088b1bd4676e.svg#O PTICubaLibreTwo") format("svg"); /* iOS 4.1- */
+}
 `;
